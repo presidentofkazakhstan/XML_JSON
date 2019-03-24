b21ea2d7888800e067a6180f96744c7c16bf6652
presidentofkazakhstan <alibekbekhan@gmail.com>
2019-03-24 23:59:40 +0600

    first commit

---
+++ Motobike.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xml_and_json
+{
+   public class Motobike
+    {
+        public string Company     { get; set; }
+        public string Model       { get; set; }
+        public double EnginePower { get; set; }
+        public int ReleaseYear    { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xml_and_json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Motobike motobike = new Motobike
+            {
+                Company = "Racer",
+                Model = "Racer Ranger RC300-GY8",
+                EnginePower = 19.7 , 
+                ReleaseYear = 2017
+            };
+
+        }
+    }
+}
